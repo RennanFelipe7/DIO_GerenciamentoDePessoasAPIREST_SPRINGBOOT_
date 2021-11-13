54985c0ab663046036901a4dda22052e1f46471e
RennanFelipe7 <rennanfbcsp@gmail.com>
2021-11-13 17:18:44 -0300

    Adicionando a notacao RestController na classe principal

--- src/main/java/one/digitalinnovation/personalapi/PersonalapiApplication.java
+++ src/main/java/one/digitalinnovation/personalapi/PersonalapiApplication.java
@@ -3,8 +3,10 @@ package one.digitalinnovation.personalapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @ComponentScan
 		public class PersonalapiApplication {
 
